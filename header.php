49484782b77309540b0dab210df61b2913019c1f
henryayuso <39566805+henryayuso@users.noreply.github.com>
2019-06-05 13:16:03 -0600

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
 </style>
 	<meta charset="utf-8">
 	<title>Login Form Design</title>
-	
+	 <link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="login.css">
 	
 </head>
