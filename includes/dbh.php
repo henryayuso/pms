18ee6cbde4d4222b289f6d72f74e8986e469cd25
henryayuso <39566805+henryayuso@users.noreply.github.com>
2019-06-11 13:49:00 -0600

    Update and rename readme to dbh.php

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "192.168.2.100";
+$dbUsername = "root";
+$dbPassword = "papi2009";
+$dbName = "users";
+
+$conn = mysqli_connect($dbServername,dbUsername,$dbPassword,$dbName);
